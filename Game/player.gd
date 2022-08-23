caba834354114510a0d5ee6752d92834911bdc34
fahreradam <72369125+fahreradam@users.noreply.github.com>
2022-08-23 09:44:07 -0400

    First steps of learning

---
+++ Game/player.gd
@@ -0,0 +1,16 @@
+extends KinematicBody
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export var speed = 11.5
+var health = 100
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("hello world")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
