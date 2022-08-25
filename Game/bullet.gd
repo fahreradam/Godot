8c54280d5460e4df31ab499a4e04fa7b4e3eb709
fahreradam <72369125+fahreradam@users.noreply.github.com>
2022-08-25 09:44:48 -0400

    Added bullet

---
+++ Game/bullet.gd
@@ -0,0 +1,20 @@
+extends Area
+
+signal bullet_killed(points)
+
+
+export var speed = 10.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	translate_object_local(Vector3(0,0,speed*delta))
+
+
+func hit_something(body):
+	print("I hit", body)
+	emit_signal("bullet_killed", 100)
+	queue_free()
--- Game/player.gd
+++ Game/player.gd
@@ -2,15 +2,71 @@ extends KinematicBody
 
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
 export var speed = 11.5
 var health = 100
+
+var color_change_delay = 3.0
+var time_to_color_change = color_change_delay
+
+var bullet_scene = preload("res://bullet.tscn")
+
+var direction_map = {"move_left": Vector3(0,0,-1),
+					 "move_right": Vector3(0,0,1),
+					 "move_up": Vector3(1,0,0),
+					 "move_down": Vector3(-1,0,0)}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("hello world")
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	do_movement()
+	
+	do_color_change(delta)
+	
+	if Input.is_action_just_pressed("fire"):
+		# Make a new (disconnected scene instance
+		var new_inst = bullet_scene.instance()
+	
+		# Make the new instance a part of the main scene
+		get_tree().get_root().add_child(new_inst)
+	
+		# rotate / translate the new instance
+		new_inst.transform.origin = transform.origin
+		new_inst.rotation_degrees = Vector3(0,90,0)
+		
+		# connect the bullet's "bullet_destroyed" signal to our my_bullet_dead method
+		new_inst.connect("bullet_killed", self, "my_bullet_is_dead")
+		
+func my_bullet_is_dead(points):
+	print("My bullet just hit something")
+		
+func do_color_change(delta):
+	time_to_color_change -= delta
+	if time_to_color_change <= 0:
+		# Reset the timer
+		time_to_color_change += color_change_delay
+		
+		# Change the color -- the interesting part
+		# here is accessing our mesh_instance node
+		var mesh_node = $player_mesh
+		
+		# Change the color of the material
+		mesh_node.get_active_material(0).albedo_color = \
+			Color(randf(), randf(), randf())
+	
+func do_movement():
+	var velocity = Vector3.ZERO
+	# Look through all the keys in our direction
+	# map and see if that action is pressed atm
+	for action in direction_map:
+		if Input.is_action_pressed(action):
+			# It is presssed -- add that value
+			# to our total
+			velocity += direction_map[action]
+	velocity = velocity.normalized()
+	
+	# Move this object and correctly hit obsticles
+	move_and_slide(velocity * speed, Vector3.UP)
