8214a8837d36246882f3968255a3381488c4a076
fahreradam <72369125+fahreradam@users.noreply.github.com>
2022-09-11 09:57:20 -0400

    Revert "added things"
    This reverts commit 45cee74421502171ba75ff41c9126e6807e29ba1.

--- Game/Enemy.gd
+++ Game/Enemy.gd
@@ -3,8 +3,6 @@ extends KinematicBody
 var direction = Vector3(0,0,0)
 export var speed = 5
 var health = 100
-var time = 0
-var end = Vector3(0,0,0)
 
 
 
@@ -22,51 +20,20 @@ func _process(delta):
 	
 	
 	var player_pos = player.global_transform.origin
-
-
-	if (Vector2(player_pos.x, player_pos.z) - Vector2(self.global_transform.origin.x, self.global_transform.origin.z)).length() <= 20:
-#		hunt(player_pos)
-		pass
-	else:
-		wander(delta)
-		
+	direction = (player_pos-self.global_transform.origin).normalized()
+	self.look_at(player_pos, Vector3.UP)
+	move_and_slide(direction * speed)
+	for i in get_slide_count():
+		var collision = get_slide_collision(i)
+		if collision.collider.name == "player":
+			player.health -= 10*delta
+	
 	current_health()
-	damage(delta)
 	
 func current_health():
 	$health_bar.set_percentage(health)
 	if health <= 0:
 		queue_free()
-
-func hunt(pos):
-	direction = (pos-self.global_transform.origin).normalized()
-	self.look_at(pos, Vector3.UP)
-	move_and_slide(direction*speed, Vector3.UP)
-
-func wander(delta):
-
-	if time <= 0:
-		end = Vector3(randi(), randi(), randi())
-		direction = (end - self.transform.origin).normalized()
-		print(end)
-		time = 3 + time
-		self.look_at(end, Vector3.UP)
-
-	else:
-		move_and_slide(direction*speed, Vector3.UP)
-		time -= delta
-	
-	for i in get_slide_count():
-		var collision = get_slide_collision(i)
-		if collision.collider.name == "obstacle":
-			
-	
-	
-func damage(delta):
-	for i in get_slide_count():
-		var collision = get_slide_collision(i)
-		if collision.collider.name == "player":
-			player.health -= 10*delta
 	
 #func _on_Timer_timeout():
 #	print("changing eyeball color")
