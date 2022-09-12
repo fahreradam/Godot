36441eef6d6594604f15d450f8d213c53a4a1b03
fahreradam <72369125+fahreradam@users.noreply.github.com>
2022-09-12 13:24:34 -0400

    Forgot to save

--- Game/Ending.gd
+++ Game/Ending.gd
@@ -4,10 +4,12 @@ extends CanvasLayer
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var prev_visible = visible
 	if get_tree().get_nodes_in_group("Player")[0].health <= 0:
 		visible = true
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	
+	if prev_visible != visible:
+		$lose_sound.play()
 func _on_quit_gui_input(event):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
 		get_tree().quit()
