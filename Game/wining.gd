64ff9181e607e74da3013e104a8d17a7e2836bcb
fahreradam <72369125+fahreradam@users.noreply.github.com>
2022-09-11 23:47:54 -0400

    forgot to save

--- Game/wining.gd
+++ Game/wining.gd
@@ -1,20 +1,14 @@
 extends CanvasLayer
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
+var time = 0
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
+func _process(delta):
+	
+	if len(get_tree().get_nodes_in_group("Enemy")) <= 0 and time <= 0:
+		get_tree().paused = true
+		visible = true
 func _on_quit_gui_input(event):
-	pass # Replace with function body.
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+		get_tree().quit()
